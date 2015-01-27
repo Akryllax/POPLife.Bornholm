7f7ae2384a09a0ad3d88a58f3107d97c4e0db6ea
Plata-O-Plomo2 <plataoplomo69@gmail.com>
2015-01-27 14:16:44 +0100

    Revert "fix"
    This reverts commit af19ca940c9ce7aa1ed68db16fd2fb11cc79544d.

--- DESARROLLO/Altis_Life.Altis/dialog/smartphone.hpp
+++ DESARROLLO/Altis_Life.Altis/dialog/smartphone.hpp
@@ -5,17 +5,24 @@ class Life_my_smartphone {
 	enableSimulation = true;
 	onLoad = "[] spawn life_fnc_smartphone;";
 	
-	class controlsBackground {		
-		class EXphone : Life_RscPicture 
-		{
-			idc = 3001;
-			text = "textures\phone.paa";
-			x = 0.384999;
-			y = -0.00433818;
-			w = 0.8125;
-			h = 1.03676;
-		};	
-	};	
+	class controlsBackground {
+		class Life_RscTitleBackground:Life_RscText {
+			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", "(profilenamespace getvariable ['GUI_BCG_RGB_A',0.7])"};
+			idc = -1;
+			x = 0.1;
+			y = 0.2;
+			w = 0.95;
+			h = (1 / 25);
+		};
+		
+	class MainBackground:Life_RscText {
+			colorBackground[] = {0, 0, 0, 0.7};
+			idc = -1;
+			x = 0.1;
+			y = 0.2 + (11 / 250);
+			w = 0.95;
+			h = 0.7 - (22 / 250);
+		};
         
     class PlayerListTitleBackground:Life_RscText {
 			colorBackground[] = {0.588, 0.424, 0.145, 1.0};
