a84e2cdeaef2b0e1ff585aefbc634ab049dff23f
Alexmontboik <alexmontboik@gmail.com>
2015-01-27 09:30:30 +0100

    Ups

--- DESARROLLO/Altis_Life.Altis/dialog/pInteraction.h
+++ DESARROLLO/Altis_Life.Altis/dialog/pInteraction.h
@@ -152,7 +152,7 @@ class pInteraction_Menu
 			h = 0.038;
 		};
 		
-		class ButtonEight : life_RscButtonMenu
+		class ButtonNine : life_RscButtonMenu
 		{
 			idc = 37458;
 			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", 0.5};
