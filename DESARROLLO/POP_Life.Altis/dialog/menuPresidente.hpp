7b61007853599ca6ef9c2020bb38ddf20c271cdf
rosenz <dahedu15@gmail.com>
2015-02-23 17:21:06 +0100

    Revert "fix"
    This reverts commit 77fe947ddb8e69d17483d74b141446e63cc2ff4d.

--- DESARROLLO/POP_Life.Altis/dialog/menuPresidente.hpp
+++ DESARROLLO/POP_Life.Altis/dialog/menuPresidente.hpp
@@ -5,104 +5,76 @@
 *
 **/
 
-class presidente
+class life_presidente_menu 
 {
-        idd = 1900;
-        name= "life_presidente_menu";
-        movingEnable = false;
-        enableSimulation = true;
-       
- 
-        class Boton1: RscButton
-                {
-                        idc = -1;
-                        text = "Legalizar Marihuana"; 
-                        onButtonClick = "[marijuana] spawn AKR_fnc_makeLegal;";
-                        x = 0.37625 * safezoneW + safezoneX;
-                        y = 0.566 * safezoneH + safezoneY;
-                        w = 0.0979687 * safezoneW;
-                        h = 0.033 * safezoneH;
-                };
-        class Boton2: RscButton
-                {
-                        idc = -1;
-                        text = "Boton2"; 
-                        x = 0.37625 * safezoneW + safezoneX;
-                        y = 0.522 * safezoneH + safezoneY;
-                        w = 0.0979687 * safezoneW;
-                        h = 0.033 * safezoneH;
-                };
-        class Boto3: RscButton
-                {
-                        idc = -1;
-                        text = "Boton3"; 
-                        x = 0.37625 * safezoneW + safezoneX;
-                        y = 0.478 * safezoneH + safezoneY;
-                        w = 0.0979687 * safezoneW;
-                        h = 0.033 * safezoneH;
-                };
-        class Boton4: RscButton
-                {
-                        idc = -1;
-                        text = "Boton4"; 
-                        x = 0.37625 * safezoneW + safezoneX;
-                        y = 0.434 * safezoneH + safezoneY;
-                        w = 0.0979687 * safezoneW;
-                        h = 0.033 * safezoneH;
-                };
-        class Boton5: RscButton
-                {
-                        idc = -1;
-                        text = "Ilegalizar Marihuana"; 
-                        onButtonClick = "[marijuana,2000] spawn AKR_fnc_makeIlegal;";
-                        x = 0.5 * safezoneW + safezoneX;
-                        y = 0.566 * safezoneH + safezoneY;
-                        w = 0.0979687 * safezoneW;
-                        h = 0.033 * safezoneH;
-                };
-        class Boton6: RscButton
-                {
-                        idc = -1;
-                        text = "Boton6"; 
-                        x = 0.5 * safezoneW + safezoneX;
-                        y = 0.522 * safezoneH + safezoneY;
-                        w = 0.0979687 * safezoneW;
-                        h = 0.033 * safezoneH;
-                };
-        class Boton7: RscButton
-                {
-                        idc = -1;
-                        text = "Boton7"; 
-                        x = 0.5 * safezoneW + safezoneX;
-                        y = 0.478 * safezoneH + safezoneY;
-                        w = 0.0979687 * safezoneW;
-                        h = 0.033 * safezoneH;
-                };
-        class Boton8: RscButton
-                {
-                        idc = -1;
-                        text = "Boton8"; 
-                        x = 0.5 * safezoneW + safezoneX;
-                        y = 0.434 * safezoneH + safezoneY;
-                        w = 0.0979687 * safezoneW;
-                        h = 0.033 * safezoneH;
-                };
-        class IGUIBack_2200: IGUIBack
-                {
-                        idc = -1;
-                        x = 0.360781 * safezoneW + safezoneX;
-                        y = 0.401 * safezoneH + safezoneY;
-                        w = 0.252656 * safezoneW;
-                        h = 0.209 * safezoneH;
-                };
-        class textopresidente: RscText
-                {
-                        idc = -1;
-                        text = "Menu del presidente"; 
-                        x = 0.432969 * safezoneW + safezoneX;
-                        y = 0.39 * safezoneH + safezoneY;
-                        w = 0.103125 * safezoneW;
-                        h = 0.044 * safezoneH;
-                };
-        };
+	idd = 1900;
+	name= "life_presidente_menu";
+	movingEnable = false;
+	enableSimulation = true;
+	
+	
+	class controlsBackground {
+		class Presi_RscTitleBackground : life_RscText {
+			colorBackground[] = {0.4, 0, 0, 0.7};
+			idc = -1;
+			x = 0.3;
+			y = 0.2;
+			w = 0.5;
+			h = (1 / 25);
+		};
+		
+		class MainBackground : life_RscText {
+			colorBackground[] = {0, 0, 0, 0.7};
+			idc = -1;
+			x = 0.3;
+			y = 0.2 + (11 / 250);
+			w = 0.5;
+			h = 0.45 - (22 / 250);
+		};	
+	};
+	
+	class controls 
+	{
+		class Title : life_RscTitle {
+			colorBackground[] = {0, 0, 0, 0};
+			idc = -1;
+			text = "Menu del presidente";
+			x = 0.3;
+			y = 0.2;
+			w = 0.8;
+			h = (1 / 25);
+		};
+		
+		class Legalizar: life_RscButtonMenu
+		{
+			idc = -1;
+			text = "Legalizar Marihuana";
+			onButtonClick = "[marijuana] spawn AKR_fnc_makeLegal;";
+			x = 0.25;
+			y = 0.45 ;
+			w = (6.25 / 40);
+			h = (1 / 25);
+		};
+		
+		class Ilegalizar: life_RscButtonMenu
+		{
+			idc = -1;
+			text = "Ilegalizar Marihuana";
+			onButtonClick = "[marijuana] spawn AKR_fnc_makeIlegal;";
+			x = 0.55;
+			y = 0.45 ;
+			w = (6.25 / 40);
+			h = (1 / 25);
+		};
+
+		class ButtonClose : life_RscButtonMenu {
+			idc = -1;
+			text = "Cerrar";
+			onButtonClick = "closeDialog 0;";
+			x = 0.48;
+			y = 0.65 - (1 / 25);
+			w = (6.25 / 40);
+			h = (1 / 25);
+		};
+	};
 };
